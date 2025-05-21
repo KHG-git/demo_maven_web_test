2113ecd5f649d54de1e1103514d46170674ef509
KHG-git <66579975+KHG-git@users.noreply.github.com>
2025-05-21 17:52:14 +0900

    Initial commit

--- src/main/java/com/example/demo_maven_web/DemoMavenWebApplication.java
+++ src/main/java/com/example/demo_maven_web/DemoMavenWebApplication.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
@@ -18,47 +19,8 @@ public class DemoMavenWebApplication {
 
 	public static void main(String[] args) {
 
-		//SpringApplication.run(DemoMavenWebApplication.class, args);
+		SpringApplication.run(DemoMavenWebApplication.class, args);
 
-		Tomcat tomcat = new Tomcat();
-		tomcat.setPort(8888);
-        tomcat.setBaseDir("tomcat-temp");
-
-
-
-        try {
-            Context context = tomcat.addContext("/", new File(".").getAbsolutePath());
-            //Context context = tomcat.addContext("", null);
-
-            HttpServlet servlet = new HttpServlet() {
-                @Override
-                protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    //super.doGet(req, resp);
-                    resp.setContentType("text/html");
-                    PrintWriter writer = resp.getWriter();
-                    writer.println("<html><head><title>");
-                    writer.println("Hey, Tomcat");
-                    writer.println("</title></head>");
-                    writer.println("<body><h1>Hello Tomcat</h1></body>");
-                    writer.println("</html>");
-                }
-            };
-
-            String servletName = "helloServlet";
-            tomcat.addServlet("/",servletName, servlet);
-            context.addServletMappingDecoded("/hello", servletName);
-
-            tomcat.start();
-            System.out.println("Tomcat started on port: " + tomcat.getConnector().getPort());
-            tomcat.getServer().await();
-        } catch (LifecycleException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-        catch (Exception e) {
-            e.printStackTrace(); // 추가
-            throw new RuntimeException(e);
-        }
 
 
 	}
